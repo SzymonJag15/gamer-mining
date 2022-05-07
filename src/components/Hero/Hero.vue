22b03199c0ff4d027b14beec744a47f45de7392d
szymon <szymon.jagielski@olicom.pl>
2022-05-07 10:10:57 +0200

    Create hero section

---
+++ src/components/Hero/Hero.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="Hero">
+    <div class="Hero__content">
+      <div class="Hero__content-image">
+        <img :src="MiningHero" alt="Hero title" class="Hero__image-text" />
+
+        <div class="Hero__content-image-titles">
+          <hero-icon-title
+            v-for="item in HERO_TITLES"
+            :key="item.title"
+            :icon="item.icon"
+            :text="item.title"
+          />
+        </div>
+      </div>
+      <div class="Hero__content-bottom">
+        <p class="Hero__content-bottom__text">
+          what can i do with earned funds?
+        </p>
+        <img
+          :src="ArrowDown"
+          alt="Arrow down"
+          class="Hero__content-bottom__arrow-down"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeroIconTitle from "@/components/Hero/HeroIconTitle/HeroIconTitle.vue";
+
+import MiningHero from "@/assets/mining-hero.png";
+import GraphIcon from "@/assets/icons/graph-icon.svg";
+import MiningIcon from "@/assets/icons/mining-icon.svg";
+import BuyIcon from "@/assets/icons/buy-icon.svg";
+import ArrowDown from "@/assets/icons/arrow-down.svg";
+
+export default {
+  name: "Hero",
+  components: {
+    HeroIconTitle,
+  },
+  setup() {
+    const HERO_TITLES = [
+      {
+        title: "how much can i earn",
+        icon: GraphIcon,
+      },
+      {
+        title: "how to mine?",
+        icon: MiningIcon,
+      },
+      {
+        title: "buy devices",
+        icon: BuyIcon,
+      },
+    ];
+
+    return {
+      MiningHero,
+      HERO_TITLES,
+      ArrowDown,
+    };
+  },
+};
+</script>
+
+<style lang="scss" src="./index.scss" scoped />
---
+++ src/components/Hero/HeroIconTitle/HeroIconTitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="HeroIconTitle">
+    <img :src="icon" alt="Icon" class="HeroIconTitle__icon" />
+    <p class="HeroIconTitle__text">{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeroIconTitle",
+  props: {
+    icon: String,
+    text: String,
+  },
+};
+</script>
+
+<style lang="scss" src="./index.scss" scoped />
