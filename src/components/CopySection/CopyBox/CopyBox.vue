736bec39633b5bfbde0920c86bef9e07c90ba84e
SzymonJag15 <69639056+SzymonJag15@users.noreply.github.com>
2022-05-07 12:29:01 +0200

    Add lottie and animation to copy box (#6)
    Co-authored-by: szymon <szymon.jagielski@olicom.pl>

--- src/components/CopySection/CopyBox/CopyBox.vue
+++ src/components/CopySection/CopyBox/CopyBox.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="CopyBox">
-    <div class="CopyBox__circle-anim"></div>
+    <div class="CopyBox__circle-anim">
+      <Vue3Lottie :animationData="CircleJSON" :height="256" :width="256" />
+    </div>
     <div class="CopyBox__copy-wrapper">
       <p class="CopyBox__copy-wrapper__title">Gamercoin (GHX)</p>
       <div class="CopyBox__copy-wrapper__text">
@@ -20,14 +22,20 @@
 </template>
 
 <script>
+import { Vue3Lottie } from "vue3-lottie";
+import "vue3-lottie/dist/style.css";
+
 import AppButton from "@/components/AppButton/AppButton";
 import CopyIcon from "@/assets/icons/copy-icon.svg";
 import MetamskIcon from "@/assets/metamsk-logo.png";
+
+import CircleJSON from "@/assets/anim/circle.json";
 
 export default {
   name: "CopyBox",
   components: {
     AppButton,
+    Vue3Lottie,
   },
   props: {
     code: String,
@@ -45,6 +53,7 @@ export default {
       CopyIcon,
       MetamskIcon,
       copyCode,
+      CircleJSON,
     };
   },
 };
