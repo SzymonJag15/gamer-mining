00f2463d59978428933a49cd13922ba9eeb7dc4f
szymon <szymon.jagielski@olicom.pl>
2022-05-07 10:40:45 +0200

    Create header layout

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
 <template>
+  <Header />
   <router-view></router-view>
 </template>
+
+<script>
+import Header from "@/components/Header/Header";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="Header">
+    <img :src="Logo" alt="Logotype" />
+    <AppButton text="Log in" @click="showLogInModal" />
+  </div>
+</template>
+
+<script>
+import AppButton from "@/components/AppButton/AppButton";
+import Logo from "@/assets/gamer-logo.png";
+
+export default {
+  name: "Header",
+  components: {
+    AppButton,
+  },
+  setup() {
+    const showLogInModal = () => {
+      console.log("Log in");
+    };
+
+    return {
+      Logo,
+      showLogInModal,
+    };
+  },
+};
+</script>
+
+<style lang="scss" src="./index.scss" scoped />
