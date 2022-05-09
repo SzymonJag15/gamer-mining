4108c544e404e136e82b1c8dfe427a8ad1fa0b8f
szymon <szymon.jagielski@olicom.pl>
2022-05-09 16:12:35 +0200

    Create select section with basic select input

---
+++ src/components/AppSelect/AppSelect.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="AppSelect">
+    <Select2
+      :options="options"
+      @select="selectEvent($event)"
+      :settings="{
+        width: '355px',
+        minimumResultsForSearch: Infinity,
+      }"
+    />
+  </div>
+</template>
+<script>
+import Select2 from "vue3-select2-component";
+
+export default {
+  name: "AppSelect",
+  components: {
+    Select2,
+  },
+  props: {
+    options: Array,
+  },
+  setup() {
+    const selectEvent = (e) => {
+      console.log(e);
+    };
+
+    return {
+      selectEvent,
+    };
+  },
+};
+</script>
+
+<style lang="scss" src="./index.scss" scoped />
