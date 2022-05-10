3a42801b3a72f6e2d0a4999135450073ac8d4e4a
szymon <szymon.jagielski@olicom.pl>
2022-05-10 18:01:23 +0200

    Fix images in lottie animation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <Header @show-log-in-modal="toggleLoginForm" />
   <router-view></router-view>
-  <AppLoginModal
-    :is-visible="loginFormVisibility"
-    @hide-modal="toggleLoginForm"
-  />
+  <Transition>
+    <AppLoginModal
+      :is-visible="loginFormVisibility"
+      @hide-modal="toggleLoginForm"
+    />
+  </Transition>
 </template>
 
 <script>
@@ -30,3 +32,15 @@ export default {
   },
 };
 </script>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
